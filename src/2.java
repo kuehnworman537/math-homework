5aa56fd7a73e70c6560784cd4dfaf53dddac8ff0
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-07 21:49:46 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Random;
+
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = new Random().nextInt(10);
+        int num2 = new Random().nextInt(10);
+        System.out.println("What is " + num1 + " + " + num2 + "?");
+    }
+}
