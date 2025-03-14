9418c873855583b9eca6ef3ea0004632f21bd13b
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-14 01:23:22 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class MathHomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+    }
+}
