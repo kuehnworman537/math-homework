064d899dedf85eb0251a0d87e4cd94110c63ba4f
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-17 21:12:00 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class MathHomework {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int a = rand.nextInt(10);
+    int b = rand.nextInt(10);
+    System.out.println(a + "+" + b + "=" + (a+b));
+  }
+}
