900ee35212373289ba3145f411782cd0462ae589
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-19 21:07:03 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,7 @@
+import java.util.*; 
+public class MyMathHomework { 
+ public static void main(String[] args) { 
+ int number = (int) (Math.random() * 10); 
+ System.out.println("The result is " + number); 
+ } 
+}
