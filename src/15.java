61c056dccbc08d49168747a96b6f92d9b91cb277
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-20 17:42:10 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of arithmetic operations and conditionals in Java
+        int num1 = 5;
+        int num2 = 3;
+        double result;
+
+        if (num1 > num2) {
+            result = num1 + num2;
+        } else {
+            result = num1 - num2;
+        }
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
