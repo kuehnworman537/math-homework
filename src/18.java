93407aafda016633d8e85b690c69d158a7d6cfec
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-23 16:27:28 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code snippet
+        int x = 5;
+        System.out.println("The value of x is " + x);
+    }
+}
