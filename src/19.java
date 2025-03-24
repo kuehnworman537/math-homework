f6559b57ebbe0af87179f93b27cc088f4867ab72
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-24 14:16:06 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of how to use mathematical operations in Java
+        int num1 = 5;
+        int num2 = 3;
+        double result = num1 + num2; // Adding two numbers
+        System.out.println("The sum is: " + result);
+
+        String str1 = "Hello, World!";
+        String str2 = "Java"; // Concatenating two strings
+        String resultString = str1 + str2; // Concatenating the strings with a plus sign
+
+        System.out.println("The concatenated string is: " + resultString);
+    }
+}
