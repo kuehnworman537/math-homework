a323c2ab4b8f2f8650790174d4902a0b80b60bff
kuehnworman537 <kuehnworman537@gmail.com>
2025-03-31 12:02:24 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + (num1 - num2));
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + (num1 * num2));
+    }
+}
