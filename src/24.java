ba3b90ea19997d4d4c61a56e87dcb48dde531201
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-03 13:38:23 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java code to demonstrate basic arithmetic operations
+        int num1 = 5;
+        int num2 = 3;
+
+        int sum = num1 + num2; // Add two numbers
+        int product = num1 * num2; // Multiply two numbers
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+    }
+}
