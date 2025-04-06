cbc842de1c634a36d6a1e24fe673ce68a1ff9313
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-06 14:16:32 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,24 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double x = 5.0;
+        int y = 10;
+        float z = 7.8f;
+
+        // Add two numbers
+        double sum = x + y;
+        System.out.println("Sum of " + x + " and " + y + " is: " + sum);
+
+        // Multiply a number by another
+        double product = x * y;
+        System.out.println("Product of " + x + " and " + y + " is: " + product);
+
+        // Subtract two numbers
+        double difference = z - y;
+        System.out.println("Difference of " + z + " and " + y + " is: " + difference);
+
+        // Divide a number by another
+        double quotient = x / y;
+        System.out.println("Quotient of " + x + " and " + y + " is: " + quotient);
+    }
+}
