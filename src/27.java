9c4dc77232d7d15e8dead10d908c9aabdaf7b517
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-07 14:31:31 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        System.out.println("The value of x is " + x);
+        System.out.println("The value of y is " + y);
+    }
+}
