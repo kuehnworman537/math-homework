4e48db4da267b429cccf6787780a18fb8f96fd7e
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-08 11:30:54 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        float z = 7.8f;
+
+        System.out.println("The value of x + y: " + (x + y));
+        System.out.println("The value of z squared: " + Math.pow(z, 2));
+
+        // Add your own code here
+    }
+}
