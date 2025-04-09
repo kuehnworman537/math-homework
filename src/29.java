d7b2864be9de0a98ca8fd872d0107f8befcaede7
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-09 00:34:06 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of an arithmetic operation in Java
+        System.out.println("Addition: " + (50 + 30));
+        System.out.println("Subtraction: " + (70 - 40));
+        System.out.println("Multiplication: " + (60 * 40));
+    }
+}
