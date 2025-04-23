54771b0c21f83d235bffe02e0fbd1494b6cd45cb
kuehnworman537 <kuehnworman537@gmail.com>
2025-04-23 10:55:29 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double x = 5;
+        System.out.println("The value of x is " + x);
+        int y = 20;
+        if (y > 10) {
+            System.out.println("y is greater than 10");
+        } else {
+            System.out.println("y is not greater than 10");
+        }
+    }
+}
